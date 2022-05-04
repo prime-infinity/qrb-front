e9148075e569ea8d54a1cc3a426cb9a3587402e1
prime-infinity <soe.a.prime@gmail.com>
2022-05-04 22:58:03 +0100

    video bg

--- src/components/index/Wrapper.jsx
+++ src/components/index/Wrapper.jsx
@@ -10,6 +10,7 @@ function Wrapper() {
     navigate("/about");
   };
 
+  // eslint-disable-next-line
   const wrapImage = () => {
     if (rest.welcomescreen) {
       return { backgroundImage: `url(${rest.welcomescreen})` };
@@ -24,42 +25,43 @@ function Wrapper() {
 
   return (
     <>
-      <div className="wrapper" style={wrapImage()}>
-        <div className="container-fluid wrapper-down">
-          <div className="row justify-content-center justify-content-md-start wr-sec">
-            <div className="col-11 col-md-6 text-white">
-              <span className="h1 q-font-weight-bold">{rest.name}</span> <br />
-              <br />
-              <div className="row">
-                <div className="col-3 q-font-weight-bold">
-                  {rest?.location && stripTease(rest.location)}
-                </div>
-                <div className="col-1 q-font-weight-bold">|</div>
-                <div className="col-4 q-font-weight-bold">{rest.year}</div>
-              </div>{" "}
-              <br />
-              <div className="row">
-                <div className="col-12">
-                  <span className="fw-bold fs-14">{rest.description}</span>
-                </div>
+      <video className="videoBg" autoPlay loop muted>
+        <source src={`/videos/vdd.mp4`} type="video/mp4" />
+      </video>
+      <div className="bg-wrapper">
+        <div className="row justify-content-center justify-content-md-start">
+          <div className="col-11 col-md-6 text-white">
+            <span className="h1 q-font-weight-bold">{rest.name}</span> <br />
+            <br />
+            <div className="row">
+              <div className="col-3 q-font-weight-bold">
+                {rest?.location && stripTease(rest.location)}
               </div>
-              <br />
-              <div className="row">
-                <div className="col-6 ps-0 pe-bt">
-                  <button
-                    onClick={toAbout}
-                    className="btn py-bt bg-them w-100 text-white fw-bold"
-                  >
-                    info
-                  </button>
-                </div>
-                <div className="col-6 pe-0 ps-bt">
-                  <Link to="/menu">
-                    <button className="btn py-bt bg-dark w-100 bg-white fw-bold">
-                      menu
-                    </button>{" "}
-                  </Link>
-                </div>
+              <div className="col-1 q-font-weight-bold">|</div>
+              <div className="col-4 q-font-weight-bold">{rest.year}</div>
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col-12">
+                <span className="fw-bold fs-14">{rest.description}</span>
+              </div>
+            </div>
+            <br />
+            <div className="row">
+              <div className="col-6 ps-0 pe-bt">
+                <button
+                  onClick={toAbout}
+                  className="btn py-bt bg-them w-100 text-white fw-bold"
+                >
+                  info
+                </button>
+              </div>
+              <div className="col-6 pe-0 ps-bt">
+                <Link to="/menu">
+                  <button className="btn py-bt bg-dark w-100 bg-white fw-bold">
+                    menu
+                  </button>{" "}
+                </Link>
               </div>
             </div>
           </div>
