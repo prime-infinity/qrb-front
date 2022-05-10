d2e76ea80cffb85e550cb8a61950afcc4512fdfb
prime-infinity <soe.a.prime@gmail.com>
2022-05-10 21:38:21 +0100

    young fixes

--- src/ui/MenuItems.jsx
+++ src/ui/MenuItems.jsx
@@ -2,7 +2,7 @@
 import Accordion from "react-bootstrap/Accordion";
 import AccordionContext from "react-bootstrap/AccordionContext";
 import { useAccordionButton } from "react-bootstrap/AccordionButton";
-import { useContext } from "react";
+import { useContext /*, useState */ } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -19,7 +19,7 @@ function MenuItems({ item }) {
 
   function CustomToggle({ eventKey, callback }) {
     const { activeEventKey } = useContext(AccordionContext);
-
+    //const [sldOpen, setSldOpe] = useState(null);
     const decoratedOnClick = useAccordionButton(
       eventKey,
       () => callback && callback(eventKey)
@@ -32,6 +32,7 @@ function MenuItems({ item }) {
         <div className="m-cat ps-0 pe-0">
           <div className="m-cat-head">
             <div
+              id={item.name}
               className={` ${viewMode && "cat-left-view"} ${isCurrentEventKey &&
                 "max-w-zero "} cat-left`}
             >
@@ -60,46 +61,6 @@ function MenuItems({ item }) {
             </div>
           </div>
         </div>
-        {/*<>
-          <div className={`${isCurrentEventKey ? "yes" : "no"} col-4 ps-0`}>
-            <img src="images/qbacon.jpg" className="img-fluid w-100" alt="" />
-          </div>
-          <div className="col-8 my-auto">
-            <div className="row">
-              <div className="col-9 ps-0">
-                <span className="menu-name">{item.name}</span>
-              </div>
-              <div className="col-3">
-                <span className="fs-14 text-secondary">${8.49}</span>
-              </div>
-              <div className="col-12 ps-0">
-                <span
-                  className="fs-14 text-secondary"
-                  style={{ verticalAlign: "text-top" }}
-                >
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                </span>
-              </div>
-              <div className="col-12 text-end">
-                <span className="pe-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="svg-icon"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-                <span className="fs-14">sold out</span>
-              </div>
-            </div>
-          </div>
-        </>*/}
       </div>
     );
   }
