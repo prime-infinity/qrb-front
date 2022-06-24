ce2fbaab1bd0fcd3b3f581ca2be756ecde7d44cd
prime-infinity <soe.a.prime@gmail.com>
2022-06-24 04:01:14 -0700

    done sub category adding

--- src/components/adds/AddCategory.jsx
+++ src/components/adds/AddCategory.jsx
@@ -43,17 +43,17 @@ function AddCategory() {
   };
 
   const handleSubSuccess = (e) => {
-    console.log(e);
+    dispatch(setRest(e));
     setSubCatAdded(true);
     setPending({ ...pending, sub: false });
-    setSubCat("");
+    setSubCat({ name: "", main: "" });
     setTimeout(() => {
       setSubCatAdded(false);
     }, 2000);
   };
 
   const addMainCat = () => {
-    console.log(mainCat);
+    //console.log(mainCat);
     setPending({ ...pending, main: true });
     setErrors({ ...pending, main: null });
 
@@ -67,11 +67,11 @@ function AddCategory() {
   };
 
   const addSubCat = () => {
-    console.log(subCat);
+    //console.log(subCat);
     setPending({ ...pending, sub: true });
     setErrors({ ...pending, sub: null });
 
-    addSubCateogory(subCat, authState.token)
+    addSubCateogory({ ...subCat, restid: rest._id }, authState.token)
       .then((res) => {
         handleSubSuccess(res);
       })
@@ -128,72 +128,51 @@ function AddCategory() {
   };
 
   const subCatSelected = (e) => {
-    console.log(e.value);
+    //console.log(e.value);
     setSubCat({ ...subCat, main: e.value });
   };
 
   return (
     <div className="col-12">
       <div className="row justify-content-center pt-3">
-        <span className="ps-0 fw-14 fw-bold">all categories</span>
-        {rest.categories.map((item, index) => (
-          <div
-            className="col-12 pos-rel border border-dark br-4 my-2 py-2 fs-14"
-            key={index}
-          >
-            <div className="row">
-              <div className="col-12">
-                {item.name}
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="svg-icon  cat-icon"
-                    style={{ right: "20%" }}
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`svg-icon cat-icon ${
-                      expand === item.id
-                        ? "rotate-icon-90"
-                        : "counter-rotate-icon"
-                    } `}
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    onClick={() => expandCat(item.id)}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </div>
-
+        {rest.categories?.length > 0 && (
+          <span className="ps-0 fw-14 fw-bold">all categories</span>
+        )}
+        {rest.categories?.length < 1 && (
+          <div className="col-12 text-center py-5">
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ width: "100px" }}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                />
+              </svg>
+            </span>
+            <div className="span text-danger fs-14 pt-3">no categories yet</div>
+          </div>
+        )}
+        {rest.categories?.length > 0 &&
+          rest.categories.map((item, index) => (
             <div
-              className={`row mt-2 ${
-                expand === item.id ? "d-block" : "d-none"
-              }`}
+              className="col-12 pos-rel border border-dark br-4 my-2 py-2 fs-14"
+              key={index}
             >
-              {item.sub.map((subb, indexx) => (
-                <div key={indexx} className="col-12 py-1 my-2 border">
-                  {subb.title}
+              <div className="row">
+                <div className="col-12">
+                  {item.name}
                   <span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="svg-icon  cat-icon"
-                      style={{ right: "2%" }}
+                      style={{ right: "20%" }}
                       viewBox="0 0 20 20"
                       fill="currentColor"
                     >
@@ -204,11 +183,56 @@ function AddCategory() {
                       />
                     </svg>
                   </span>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`svg-icon cat-icon ${
+                        expand === item._id
+                          ? "rotate-icon-90"
+                          : "counter-rotate-icon"
+                      } `}
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      onClick={() => expandCat(item._id)}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
                 </div>
-              ))}
+              </div>
+
+              <div
+                className={`row mt-2 ${
+                  expand === item._id ? "d-block" : "d-none"
+                }`}
+              >
+                {item.sub.map((subb, indexx) => (
+                  <div key={indexx} className="col-12 py-1 my-2 border">
+                    {subb.name}
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="svg-icon  cat-icon"
+                        style={{ right: "2%" }}
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
 
         <div className="col-12 px-0 mt-5">
           <span className="ps-0 fw-14 fw-bold">add main category</span>
