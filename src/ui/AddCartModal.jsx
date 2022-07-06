815c5cbba6a723072baff822647b47bb072b4f60
prime-infinity <soe.a.prime@gmail.com>
2022-07-06 08:22:47 -0700

    edited category adding modal

--- src/ui/AddCartModal.jsx
+++ src/ui/AddCartModal.jsx
@@ -4,11 +4,18 @@ function AddCartModal({ close }) {
   return (
     <div
       className="custom-modal"
-      style={{ top: "5%", height: "fit-content", border: "2px solid black" }}
+      style={{
+        top: "5%",
+        height: "80vh",
+        overflowY: "scroll",
+        border: "2px solid black",
+      }}
     >
       <div className="container-fluid big-bg-theme px-3">
         <div className="row py-5">
-          <AddCategory />
+          <div className="col-12">
+            <AddCategory />
+          </div>
         </div>
       </div>
     </div>
