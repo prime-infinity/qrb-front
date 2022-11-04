9e4e8c4966ff690ca76cb37cc3037730d42512a1
prime-infinity <soe.a.prime@gmail.com>
2022-11-04 16:22:52 -0700

    minor ui fix

--- src/ui/ResOwnerMobMenu.jsx
+++ src/ui/ResOwnerMobMenu.jsx
@@ -117,7 +117,7 @@ function ResOwnerMobMenu({ closeMenu }) {
             className=""
             style={{
               position: "absolute",
-              bottom: "17%",
+              bottom: "15%",
               width: "90%",
               left: "5%",
             }}
@@ -127,18 +127,6 @@ function ResOwnerMobMenu({ closeMenu }) {
               className="btn py-2 w-100 bg-them text-white  fw-bold"
             >
               start now
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="svg-icon-lg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
             </button>
           </div>
         </div>
