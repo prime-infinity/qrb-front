abf2186108ad919067e8ced2d1532d2d0b2dc259
prime-infinity <soe.a.prime@gmail.com>
2022-12-12 03:58:16 -0800

    decided to leave scroll behavior

--- src/components/menu/AddMenuItem.jsx
+++ src/components/menu/AddMenuItem.jsx
@@ -126,73 +126,73 @@ function AddMenuItem({ details }) {
             {/** in below */}
             <div className="m-cat-head">
               {/** this very below */}
-              <div style={{ width: "100%" }} className="d-flex new-cat-right">
-                <div
-                  style={{ justifyContent: "space-between", width: "100%" }}
-                  className="cat-head  px-3 pt-3 d-flex"
-                >
-                  {isEditn() ? (
-                    <input
-                      onChange={setName}
-                      value={editnMen.name}
-                      style={{ width: "65%" }}
-                      type="text"
-                      placeholder="name"
-                      autoFocus
-                      className="cat-name-input big-bg-theme fs-14 ps-2"
-                    />
-                  ) : (
-                    <h4
-                      onClick={strEdit}
-                      className="text-secondary text-decoration-underline"
-                      style={{ marginBottom: "5px" }}
-                    >
-                      {editnMen?.name?.length > 0 && editnMen?.isSet
-                        ? editnMen.name
-                        : "name"}
-                    </h4>
-                  )}
-                  {isEditn() ? (
-                    <input
-                      onChange={setPrice}
-                      value={editnMen.price}
-                      style={{ width: "25%" }}
-                      type="number"
-                      placeholder="price($)"
-                      className="cat-name-input big-bg-theme fs-14 ps-2"
-                    />
-                  ) : (
-                    <span
-                      onClick={strEdit}
-                      className="price text-secondary text-decoration-underline"
-                    >
-                      {editnMen?.price?.length > 0 && editnMen?.isSet
-                        ? "$" + editnMen.price
-                        : "$price"}
-                    </span>
-                  )}
-                  <div className="d-none">
+              <div className=" cat-right ">
+                <>
+                  <div
+                    style={{ justifyContent: "space-between" }}
+                    className="cat-head d-flex"
+                  >
                     {isEditn() ? (
-                      <textarea
-                        onChange={setDesc}
-                        value={editnMen.description}
-                        style={{ marginTop: "3%" }}
+                      <input
+                        onChange={setName}
+                        value={editnMen.name}
+                        style={{ width: "65%" }}
                         type="text"
-                        placeholder="description"
+                        placeholder="name"
+                        autoFocus
                         className="cat-name-input big-bg-theme fs-14 ps-2"
                       />
                     ) : (
-                      <p
+                      <h4
                         onClick={strEdit}
                         className="text-secondary text-decoration-underline"
+                        style={{ marginBottom: "5px" }}
                       >
-                        {editnMen?.description?.length > 0 && editnMen?.isSet
-                          ? editnMen.description
-                          : "description"}
-                      </p>
+                        {editnMen?.name?.length > 0 && editnMen?.isSet
+                          ? editnMen.name
+                          : "name"}
+                      </h4>
+                    )}
+                    {isEditn() ? (
+                      <input
+                        onChange={setPrice}
+                        value={editnMen.price}
+                        style={{ width: "25%" }}
+                        type="number"
+                        placeholder="price($)"
+                        className="cat-name-input big-bg-theme fs-14 ps-2"
+                      />
+                    ) : (
+                      <span
+                        onClick={strEdit}
+                        className="price text-secondary text-decoration-underline"
+                      >
+                        {editnMen?.price?.length > 0 && editnMen?.isSet
+                          ? "$" + editnMen.price
+                          : "$price"}
+                      </span>
                     )}
                   </div>
-                </div>
+                  {isEditn() ? (
+                    <textarea
+                      onChange={setDesc}
+                      value={editnMen.description}
+                      style={{ marginTop: "3%" }}
+                      type="text"
+                      placeholder="description"
+                      className="cat-name-input big-bg-theme fs-14 ps-2"
+                    />
+                  ) : (
+                    <p
+                      onClick={strEdit}
+                      className="text-secondary text-decoration-underline"
+                    >
+                      {editnMen?.description?.length > 0 && editnMen?.isSet
+                        ? editnMen.description
+                        : "description"}
+                    </p>
+                  )}
+                </>
               </div>
             </div>
           </div>
